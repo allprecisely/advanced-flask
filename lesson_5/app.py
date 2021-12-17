240ed9ab39ab90aa2f1127c8b7e0d8f61b19f372
allprecisely <allprecisely@gmail.com>
2021-12-17 13:55:36 +0300

    move students > items and extend db

--- lesson_5/app.py
+++ lesson_5/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
-from flask_jwt import JWT, jwt_required
-from flask_restful import Resource, Api, reqparse
+from flask import Flask
+from flask_jwt import JWT
+from flask_restful import Api
 
 from security import authenticate, identity
+from items import Items, ItemsList
 from user import UserRegister
 
 app = Flask(__name__)
@@ -11,37 +12,9 @@ app.secret_key = 'ilnar'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-students = []
-
-
-class Student(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('book', type=str, required=True, help='Book cannot be blank!')
-
-    @jwt_required()
-    # curl -X POST -H "Content-Type: application/json" 127.0.0.1:5000/auth -d '{"username":"bob", "password": "asdf"}'
-    # curl 127.0.0.1:5000/student/ilnar -H "Authorization: JWT token"
-    def get(self, name):
-        student = next(filter(lambda x: x['name'] == name, students), None)
-        return {'student': student}, 200 if student else 400
-
-    @jwt_required()
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, students), None):
-            return {'message': f'Student {name} is already exists.'}
-        item = {'name': name, 'book': self.parser.parse_args()['book']}
-        students.append(item)
-        return item, 201
-
-
-class Students(Resource):
-    def get(self):
-        return students, 200
-
-
-# curl -X POST -H "Content-Type: application/json" 127.0.0.1:5000/student/ilnar -d '{"book":"hello, world!"}'
-api.add_resource(Student, '/student/<string:name>')
-api.add_resource(Students, '/students')
+api.add_resource(Items, '/items/<string:name>')
+api.add_resource(ItemsList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run()
+if __name__ == '__main__':
+    app.run()
