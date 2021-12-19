b18b209fd77be8418788646a2fe68e3a2829722e
allprecisely <allprecisely@gmail.com>
2021-12-19 17:57:44 +0300

    lessons7-8 deploying

---
+++ deploy/resources/user.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource, reqparse
+
+from models.user import UserModel
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help='username should be defined')
+    parser.add_argument('password', type=str, required=True, help='password should be defined')
+
+    def post(self):
+        data = self.parser.parse_args()
+        if UserModel.get_user_by_username(data['username']):
+            return {'message': 'User with such username is already exists'}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+        return {'message': 'User successfully created.'}, 201
