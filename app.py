3a01a38b870fbb6871701538eb8707658247c435
allprecisely <allprecisely@gmail.com>
2021-12-21 16:36:11 +0300

    url > uri

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.store import Store, StoreList
 from resources.user import UserRegister
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ilnar'
 
