c0845e3791243fe09b9c4c10d12c3cf316c57128
allprecisely <allprecisely@gmail.com>
2021-12-21 16:46:50 +0300

    workaround issue about conflicting sqlalchemy and heroku

--- app.py
+++ app.py
@@ -10,21 +10,26 @@ from resources.store import Store, StoreList
 from resources.user import UserRegister
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'ilnar'
+# https://stackoverflow.com/questions/66690321
+database_url = os.getenv("DATABASE_URI", "sqlite:///data.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url.replace(
+    "postgres://", "postgresql://", 1
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "ilnar"
 
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Item, '/items/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
-api.add_resource(Store, '/stores/<string:name>')
-api.add_resource(StoreList, '/stores')
+api.add_resource(Item, "/items/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
+api.add_resource(Store, "/stores/<string:name>")
+api.add_resource(StoreList, "/stores")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from db import db
+
     db.init_app(app)
     app.run()
